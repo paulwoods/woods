613a509be7157d829421476d8ebf12c78437574f
Paul Woods <mr.paul.woods@gmail.com>
2024-02-18 10:09:01 -0600

    refactor: UPDATED auth server

--- auth/src/main/java/org/mrpaulwoods/auth/SecurityConfig.java
+++ auth/src/main/java/org/mrpaulwoods/auth/SecurityConfig.java
@@ -15,6 +15,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
@@ -26,6 +28,7 @@ import org.springframework.security.oauth2.server.authorization.config.annotatio
 import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
 import org.springframework.security.oauth2.server.authorization.settings.AuthorizationServerSettings;
 import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
@@ -55,14 +58,16 @@ public class SecurityConfig {
                 // Redirect to the login page when not authenticated from the
                 // authorization endpoint
                 .exceptionHandling((exceptions) -> exceptions
-                        .defaultAuthenticationEntryPointFor(
-                                new LoginUrlAuthenticationEntryPoint("/login"),
-                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
-                        )
-                )
+//                        .defaultAuthenticationEntryPointFor(
+//                                new LoginUrlAuthenticationEntryPoint("/login"),
+//                                new MediaTypeRequestMatcher(MediaType.TEXT_HTML)
+//                        )
+                                .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                );
+
                 // Accept access tokens for User Info and/or Client Registration
-                .oauth2ResourceServer((resourceServer) -> resourceServer
-                        .jwt(Customizer.withDefaults()));
+//                .oauth2ResourceServer((resourceServer) -> resourceServer
+//                        .jwt(Customizer.withDefaults()));
 
         return http.build();
     }
@@ -73,46 +78,51 @@ public class SecurityConfig {
             throws Exception {
 
         http
+                .formLogin(Customizer.withDefaults())
+
                 .authorizeHttpRequests((authorize) -> authorize
                         .anyRequest().authenticated()
-                )
-                // Form login handles the redirect to the login page from the
-                // authorization server filter chain
-                .formLogin(Customizer.withDefaults());
+                );
 
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService() {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
+        UserDetails user = User.withUsername("user")
                 .password("password")
-                .roles("USER")
+                .authorities("read")
+//                .roles("USER")
                 .build();
 
         return new InMemoryUserDetailsManager(user);
     }
 
     @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
     public RegisteredClientRepository registeredClientRepository() {
         RegisteredClient oidcClient = RegisteredClient.withId(UUID.randomUUID().toString())
                 .clientId("oidc-client")
-                .clientSecret("{noop}secret")
+                .clientSecret("secret")
                 .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
                 .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
-                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
+//                .authorizationGrantType(AuthorizationGrantType.REFRESH_TOKEN)
                 .redirectUri("http://127.0.0.1:8080/login/oauth2/code/oidc-client")
                 .redirectUri("https://oidcdebugger.com/debug")
                 .redirectUri("https://oauthdebugger.com/debug")
                 .redirectUri("https://springone.io/authorized")
-                .postLogoutRedirectUri("http://127.0.0.1:8080/")
+//                .postLogoutRedirectUri("http://127.0.0.1:8080/")
                 .scope(OidcScopes.OPENID)
-                .scope(OidcScopes.PROFILE)
-                .clientSettings(ClientSettings.builder()
-                        .requireAuthorizationConsent(true)
-                        .requireProofKey(false)
-                        .build())
+//                .scope(OidcScopes.PROFILE)
+//                .scope("read")
+//                .clientSettings(ClientSettings.builder()
+//                        .requireAuthorizationConsent(false)
+//                        .requireProofKey(false)
+//                        .build())
                 .build();
 
         return new InMemoryRegisteredClientRepository(oidcClient);
@@ -121,6 +131,11 @@ public class SecurityConfig {
     @Bean
     public AuthorizationServerSettings authorizationServerSettings() {
         return AuthorizationServerSettings.builder().build();
+    }
+
+    @Bean
+    public TokenSettings tokenSettings() {
+        return TokenSettings.builder().build();
     }
 
     @Bean
--- server/src/main/java/org/mrpaulwoods/server/HomeController.java
+++ server/src/main/java/org/mrpaulwoods/server/HomeController.java
@@ -3,6 +3,7 @@ package org.mrpaulwoods.server;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,9 @@ import java.util.Map;
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<Map<String,String>> greeting() {
+    public ResponseEntity<Map<String,String>> greeting(Authentication authentication) {
         log.info("greeting()");
-        return ResponseEntity.ok(Map.of("message", "Hello World"));
+        return ResponseEntity.ok(Map.of("message", "Hello " + authentication.getName()));
     }
 
 }
